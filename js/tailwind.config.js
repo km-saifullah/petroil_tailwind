9cc09c0881422efc9514a87c418b6e5df4d1e85f
km-saifullah <kmsaifullah16@gmail.com>
2023-10-07 18:56:47 +0600

    Initial project setup done

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "1144px",
+      },
+      colors: {
+        primary: "#F40404",
+        headerBG: "#282828",
+        paragraphColo: "#6C6C6C",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+    },
+  },
+};
